ceba646b52b31b142bcd22db8bc7e826a303586e
KimChansoo <kimchan1221@naver.com>
2025-02-06 23:27:17 +0900

    c1

---
+++ src/main/java/com/csk/learnspringframework/examples/c1/BusinessCalculationService.java
@@ -0,0 +1,19 @@
+package com.csk.learnspringframework.examples.c1;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class BusinessCalculationService {
+    private DataService dataService;
+    public BusinessCalculationService(@Qualifier("mongoDbDataService") DataService dataService) {
+        super();
+        this.dataService = dataService;
+    }
+    public int findMax() {
+        return Arrays.stream(dataService.retrieveData())
+                .max().orElse(0);
+    }
+}
