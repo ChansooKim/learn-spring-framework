9e407db359cab90b0afa04c4862c8d4b818e25c4
KimChansoo <kimchan1221@naver.com>
2025-02-15 23:02:25 +0900

    spring stereotype annotation

--- src/main/java/com/csk/learnspringframework/examples/c1/BusinessCalculationService.java
+++ src/main/java/com/csk/learnspringframework/examples/c1/BusinessCalculationService.java
@@ -2,10 +2,12 @@ package com.csk.learnspringframework.examples.c1;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 
-@Component
+//@Component
+@Service
 public class BusinessCalculationService {
     private DataService dataService;
     public BusinessCalculationService(@Qualifier("mongoDbDataService") DataService dataService) {
